1fb6e755319271f7180cac083ebc23260d2a47e7
maczokni <maczokni@gmail.com>
2018-03-11 10:49:19 +0000

    added suggestion to save coords and also reactive text (needs to be prettified)
    I added the text output under the map that changes when you click on a
    point. It needs to be made prettier, either by looking at the box() for
    uioutput in shiny, or just writing own HTML.
    
    I also added some suggestions to maybe once the points have been
    geocoded once, we could save the lat/long columns and write back into
    the google sheets, so that it doesn’t need to be geocoded each time
    someone loads the app?

--- Code_map.R
+++ Code_map.R
@@ -19,20 +19,39 @@ colnames(Project)[9] <- "intro"
 colnames(Project)[10] <- "methods"
 colnames(Project)[11] <- "publications"
 colnames(Project)[12] <- "website"
+colnames(Project)[13] <- "coords"
+colnames(Project)[14] <- "lat"
+colnames(Project)[15] <- "long"
 
-Project$coords <- geocode(as.character(Project$pi_city), source = "dsk")
+#check if we didn't already geocode and save back to the google docs
+Project$coords <- ifelse(is.na(Project$coords), geocode(as.character(Project$pi_city), source = "dsk", Project$coords))
 
 Project$lat <- as.numeric(Project$coords$lat)
 Project$long <- as.numeric(Project$coords$lon)
+
+#write this column back to the goole docs, so we don't have to geocode everything every time someone loads the app
+#figure out how to write back to cols in the google docs
+#probably making use of gs_edit_cells() ?
+
 
 ui <- bootstrapPage(
   tags$style(type = "text/css", "html, body {width:100%;height:100%}"),
   leafletOutput("map", width = "100%", height = "80%"),
   #put textOutput() here with 
+  uiOutput("selected_proj"),
   absolutePanel(top = 10, right = 10)
 )
 
 server <- function(input, output, session) {
+  
+  #create the text output with HTML 
+  output$selected_proj <- renderUI({
+    HTML(paste0(
+      '<td valign="top" style = "padding-left:30px; padding-top:15px;">',
+      "Click on a project on the map for more information",
+      "</td>"
+    ))
+  })
   
   output$map <- renderLeaflet({
     
@@ -50,6 +69,29 @@ server <- function(input, output, session) {
                                        "<br> <b> Other researchers: </b>", coi_names,
                                        "<br> <b> Research level: </b>", level),
                        radius = 10)
+  })
+  
+  observeEvent(input$map_marker_click, {
+    click <- input$map_marker_click
+    sel_project <- Project[which(Project$lat == click$lat & Project$long == click$lng),]
+    #updateTextInput(session, "selected_proj", paste0(
+    output$selected_proj <- renderUI({
+      HTML(paste0(
+        '<td valign="top" style = "padding-left:30px; padding-top:15px;">',
+        "<b>Project title:</b> ", sel_project$title, 
+        "<br> <b> PI institution </b>", sel_project$pi_institute,
+        "<br> <b> PI name: </b>", sel_project$pi_name,
+        "<br> <b> Other researchers: </b>", sel_project$coi_names,
+        "<br> <b> Other researchers institutes: </b>", sel_project$coi_institutes,
+        "<br> <b> Research level: </b>", sel_project$level,
+        "<br> <b> Introduction: </b>", sel_project$intro,
+        "<br> <b> Methodology: </b>", sel_project$methods,
+        "<br> <b> Reports and publications: </b>", sel_project$publications,
+        "<br> <b> Website: </b>", sel_project$website, 
+        "</td>"
+      ))
+    })
+    
   })
   
 ##Add text output "<b>Project title:</b> ", title, 
